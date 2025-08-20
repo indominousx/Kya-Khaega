8f2cea0609820fbb6c33c6db8216436806f05c5d
Krishna Gandhi <krishnagandhi020@gmail.com>
2025-08-21 00:50:53 +0530

    Simple Deployment

--- backend/app.py
+++ backend/app.py
@@ -79,4 +79,5 @@ def get_recommendations():
     return jsonify(recommendations.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
